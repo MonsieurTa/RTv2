d3b1f97c5ee17e29873b05456e0c2561c4362bd8
William TA <wta@e2r10p16.42.fr>
2019-03-02 18:45:15 +0100

    free better, free smarter

--- srcs/destroy_rtv1.c
+++ srcs/destroy_rtv1.c
@@ -6,7 +6,7 @@
 /*   By: wta <wta@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 13:53:32 by wta               #+#    #+#             */
-/*   Updated: 2019/02/28 13:57:54 by wta              ###   ########.fr       */
+/*   Updated: 2019/03/02 18:43:48 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	lst_destroy(t_lst *lst)
 	{
 		tmp = node;
 		node = node->next;
-		free(node);
+		free(tmp);
 	}
 	lst->head = NULL;
 	lst->tail = NULL;
